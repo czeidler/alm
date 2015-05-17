4bcc8c1303dee39fd51887bb02a197beb3b23a23
Clemens <czei002@aucklanduni.ac.nz>
2015-05-18 11:48:48 +1200

    Improve test build.

--- swing/src/test/java/nz/ac/auckland/alm/swing/test/SwingTests.java
+++ swing/src/test/java/nz/ac/auckland/alm/swing/test/SwingTests.java
@@ -7,15 +7,14 @@
  */
 package nz.ac.auckland.alm.swing.test;
 
-import nz.ac.auckland.alm.XTab;
-import nz.ac.auckland.alm.YTab;
+import nz.ac.auckland.alm.*;
 import nz.ac.auckland.alm.swing.ALMLayout;
 
 import javax.swing.*;
 
 
 public class SwingTests {
-    static public void testThreeButtons() throws Exception {
+    static public void testThreeButtons() {
         JDialog dialog = new JDialog();
         ALMLayout almLayout = new ALMLayout();
         dialog.setLayout(almLayout);
@@ -28,16 +27,24 @@ public class SwingTests {
         XTab x1 = almLayout.addXTab();
         XTab x2 = almLayout.addXTab();
 
-        dialog.add(new JButton("Button 1"), new ALMLayout.LayoutParams(left, top, x1, bottom));
-        dialog.add(new JButton("Button 2"), new ALMLayout.LayoutParams(x1, top, x2, bottom));
-        dialog.add(new JButton("Button 3"), new ALMLayout.LayoutParams(x2, top, right, bottom));
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
+
+        dialog.add(button1, new ALMLayout.LayoutParams(left, top, x1, bottom));
+        dialog.add(button2, new ALMLayout.LayoutParams(x1, top, x2, bottom));
+        dialog.add(button3, new ALMLayout.LayoutParams(x2, top, right, bottom));
+
+        almLayout.areaOf(button1).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(button2).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(button3).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
 
         dialog.pack();
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
     }
 
-    static public void testPinWheel() throws Exception {
+    static public void testPinWheel() {
         JDialog dialog = new JDialog();
         ALMLayout almLayout = new ALMLayout();
         dialog.setLayout(almLayout);
@@ -54,11 +61,23 @@ public class SwingTests {
         YTab y1 = almLayout.addYTab();
         YTab y2 = almLayout.addYTab();
 
-        dialog.add(new JButton("Button 1"), new ALMLayout.LayoutParams(left, top, x2, y1));
-        dialog.add(new JButton("Button 2"), new ALMLayout.LayoutParams(x2, top, right, y2));
-        dialog.add(new JButton("Button 3"), new ALMLayout.LayoutParams(x1, y2, right, bottom));
-        dialog.add(new JButton("Button 4"), new ALMLayout.LayoutParams(left, y1, x1, bottom));
-        dialog.add(new JButton("Middle"), new ALMLayout.LayoutParams(x1, y1, x2, y2));
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        JButton button3 = new JButton("Button 3");
+        JButton button4 = new JButton("Button 4");
+        JButton buttonMiddle = new JButton("Middle");
+
+        dialog.add(button1, new ALMLayout.LayoutParams(left, top, x2, y1));
+        dialog.add(button2, new ALMLayout.LayoutParams(x2, top, right, y2));
+        dialog.add(button3, new ALMLayout.LayoutParams(x1, y2, right, bottom));
+        dialog.add(button4, new ALMLayout.LayoutParams(left, y1, x1, bottom));
+        dialog.add(buttonMiddle, new ALMLayout.LayoutParams(x1, y1, x2, y2));
+
+        almLayout.areaOf(button1).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(button2).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(button3).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(button4).setAlignment(HorizontalAlignment.FILL, VerticalAlignment.FILL);
+        almLayout.areaOf(buttonMiddle).setAlignment(HorizontalAlignment.CENTER, VerticalAlignment.CENTER);
 
         dialog.pack();
         dialog.setLocationRelativeTo(null);
@@ -66,11 +85,7 @@ public class SwingTests {
     }
 
     public static void main(String[] args) {
-        try {
-            testThreeButtons();
-            testPinWheel();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        testThreeButtons();
+        testPinWheel();
     }
 }
