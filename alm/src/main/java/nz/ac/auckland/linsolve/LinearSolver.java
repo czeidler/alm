32950bef64e2ff74ab510c84c4ca5ff0690688bb
Clemens <czei002@aucklanduni.ac.nz>
2015-05-20 14:18:59 +1200

    Clean up interface.

--- alm/src/main/java/nz/ac/auckland/linsolve/LinearSolver.java
+++ alm/src/main/java/nz/ac/auckland/linsolve/LinearSolver.java
@@ -9,47 +9,47 @@ public interface LinearSolver {
     /**
      * returns linear spec which is used by this solver.
      */
-    public LinearSpec getLinearSpec();
+    LinearSpec getLinearSpec();
 
     /**
      * Replaces the linear spec which is used by this solver.
      */
-    public void setLinearSpec(LinearSpec linearSpec);
+    void setLinearSpec(LinearSpec linearSpec);
 
-    public void presolve();
+    void presolve();
 
     /**
      * Remove a cached presolved model, if existent.
      * This is automatically done each time after the model has been changed,
      * to avoid an old cached presolved model getting out of sync.
      */
-    public void removePresolved();
+    void removePresolved();
 
     /**
      * Adds a constraint to this solver. Some solvers keep their own set of constraints.
      */
 
-    public void add(Constraint c);
+    void add(Constraint c);
 
     /**
      * Updates a constraint in this solver. Some solvers keep their own set of constraints.
      */
-    public void update(Constraint c);
+    void update(Constraint c);
 
     /**
      * Removes a constraint from this solver. Some solvers keep their own set of constraints.
      */
-    public void remove(Constraint c);
+    void remove(Constraint c);
 
     /**
      * Adds a variable to this solver.
      */
-    public void add(Variable v);
+    void add(Variable v);
 
     /**
      * Removes a variable from this solver.
      */
-    public void remove(Variable v);
+    void remove(Variable v);
 
     /**
      * Tries to solve the linear problem.
@@ -57,19 +57,19 @@ public interface LinearSolver {
      *
      * @return the result of the solving attempt
      */
-    public ResultType solve();
+    ResultType solve();
 
     /**
      * Total time of last solving attempt in milliseconds.
      *
      * @return time in milliseconds
      */
-    public long getLastSolvingTime();
+    long getLastSolvingTime();
 
     /**
      * Result of last solving attempt (e.g. OPTIMAL, INFEASIBLE, ...).
      *
      * @return result
      */
-    public ResultType getLastSolvingResult();
+    ResultType getLastSolvingResult();
 }
