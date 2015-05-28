374166035f12af4e79da8272c738b7381b76864b
Clemens <czei002@aucklanduni.ac.nz>
2015-05-29 11:24:23 +1200

    Add LinSolve test.

---
+++ alm/src/test/java/nz/ac/auckland/alm/test/LinSolve.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package nz.ac.auckland.alm.test;
+
+import junit.framework.TestCase;
+import nz.ac.auckland.linsolve.Constraint;
+import nz.ac.auckland.linsolve.LinearSpec;
+import nz.ac.auckland.linsolve.OperatorType;
+import nz.ac.auckland.linsolve.Variable;
+
+
+public class LinSolve extends TestCase {
+    public void testInternal() {
+        LinearSpec linearSpec = new LinearSpec();
+
+        Constraint constraint = linearSpec.addConstraint(1, new Variable(), 2, new Variable(), OperatorType.EQ, 10);
+
+        assertEquals(2, linearSpec.getVariables().size());
+        assertEquals(1, linearSpec.getConstraints().size());
+
+        linearSpec.removeConstraint(constraint);
+
+        assertEquals(0, linearSpec.getVariables().size());
+        assertEquals(0, linearSpec.getConstraints().size());
+    }
+}
