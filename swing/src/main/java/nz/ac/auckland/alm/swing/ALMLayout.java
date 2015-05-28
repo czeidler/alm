a09127189eb7c7074847e6f10872314a67abb928
Clemens <czei002@aucklanduni.ac.nz>
2015-05-29 11:28:20 +1200

    Move LayoutStyleType to the swing ALMLayout.

--- swing/src/main/java/nz/ac/auckland/alm/swing/ALMLayout.java
+++ swing/src/main/java/nz/ac/auckland/alm/swing/ALMLayout.java
@@ -31,6 +31,16 @@ public class ALMLayout implements LayoutManager2 {
         }
     }
 
+    /**
+     * The possibilities for adjusting a GUI's layout.
+     * Either change the child controls so that they fit into their parent control,
+     * or adjust the size of the parent control so that the children have as much space
+     * as they want.
+     */
+    enum LayoutStyleType {
+        FIT_TO_SIZE, ADJUST_SIZE
+    }
+
     private boolean activated = true;
 
     /** The specification used for calculating the layout. */
