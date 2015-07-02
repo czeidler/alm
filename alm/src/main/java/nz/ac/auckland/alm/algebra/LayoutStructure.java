b323cc86e2e3ee692144ddcddbf40034c51b9d8f
Clemens Zeidler <czei002@aucklanduni.ac.nz>
2015-07-02 15:59:17 +1200

    Clean up the LayoutStructure.

--- alm/src/main/java/nz/ac/auckland/alm/algebra/LayoutStructure.java
+++ alm/src/main/java/nz/ac/auckland/alm/algebra/LayoutStructure.java
@@ -18,19 +18,12 @@ public class LayoutStructure {
   final YTab top;
   final XTab right;
   final YTab bottom;
-  final Map<XTab, Edge> xTabEdgeMap = new HashMap<XTab, Edge>();
-  final Map<YTab, Edge> yTabEdgeMap = new HashMap<YTab, Edge>();
-  List<XTab> xTabs;
-  List<YTab> yTabs;
-  final List<Area> areas = new ArrayList<Area>();
-  final List<EmptySpace> emptySpaces = new ArrayList<EmptySpace>();
-
-  public LayoutStructure(XTab left, YTab top, XTab right, YTab bottom) {
-    this.left = left;
-    this.top = top;
-    this.right = right;
-    this.bottom = bottom;
-  }
+  final Map<XTab, Edge> xTabEdgeMap;
+  final Map<YTab, Edge> yTabEdgeMap;
+  List<XTab> sortedXTabs;
+  List<YTab> sortedYTabs;
+  final List<Area> areas;
+  final List<EmptySpace> emptySpaces;
 
   Comparator<Variable> tabComparator = new Comparator<Variable>() {
     @Override
@@ -39,11 +32,31 @@ public class LayoutStructure {
     }
   };
 
+  public LayoutStructure(XTab left, YTab top, XTab right, YTab bottom) {
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+
+    this.xTabEdgeMap = new HashMap<XTab, Edge>();
+    this.yTabEdgeMap = new HashMap<YTab, Edge>();
+
+    this.areas = new ArrayList<Area>();
+    this.emptySpaces = new ArrayList<EmptySpace>();
+
+  }
+
   public LayoutStructure(LayoutSpec layoutSpec, Area removedArea) {
     this.left = layoutSpec.getLeft();
     this.top = layoutSpec.getTop();
     this.right = layoutSpec.getRight();
     this.bottom = layoutSpec.getBottom();
+
+    this.xTabEdgeMap = new HashMap<XTab, Edge>();
+    this.yTabEdgeMap = new HashMap<YTab, Edge>();
+
+    this.areas = new ArrayList<Area>();
+    this.emptySpaces = new ArrayList<EmptySpace>();
 
     for (IArea area : layoutSpec.getAreas()) {
       if (area == removedArea)
@@ -134,42 +147,26 @@ public class LayoutStructure {
     return yTabEdgeMap;
   }
 
-  public XTab findTabLeftOf(double x) {
-    return findFirstSmallerTab(x, getXTabs());
+  public List<XTab> getSortedXTabs() {
+    if (sortedXTabs != null)
+      return sortedXTabs;
+    sortedXTabs = new ArrayList<XTab>(xTabEdgeMap.keySet());
+    Collections.sort(sortedXTabs, tabComparator);
+    return sortedXTabs;
   }
 
-  public XTab findTabRightOf(double x) {
-    return findFirstLargerTab(x, getXTabs());
-  }
+  public List<YTab> getSortedYTabs() {
+    if (sortedYTabs != null)
+      return sortedYTabs;
 
-  public YTab findTabAbove(double y) {
-    return findFirstSmallerTab(y, getYTabs());
-  }
-
-  public YTab findTabBellow(double y) {
-    return findFirstLargerTab(y, getYTabs());
-  }
-
-  public List<XTab> getXTabs() {
-    if (xTabs != null)
-      return xTabs;
-    xTabs = new ArrayList<XTab>(xTabEdgeMap.keySet());
-    Collections.sort(xTabs, tabComparator);
-    return xTabs;
-  }
-
-  public List<YTab> getYTabs() {
-    if (yTabs != null)
-      return yTabs;
-
-    yTabs = new ArrayList<YTab>(yTabEdgeMap.keySet());
-    Collections.sort(yTabs, tabComparator);
-    return yTabs;
+    sortedYTabs = new ArrayList<YTab>(yTabEdgeMap.keySet());
+    Collections.sort(sortedYTabs, tabComparator);
+    return sortedYTabs;
   }
 
   private void invalidateTabs() {
-    xTabs = null;
-    yTabs = null;
+    sortedXTabs = null;
+    sortedYTabs = null;
   }
 
   public XTab getLeft() {
@@ -186,39 +183,5 @@ public class LayoutStructure {
 
   public YTab getBottom() {
     return bottom;
-  }
-
-  private <Tab extends Variable> Tab findFirstLargerTab(double value, List<Tab> tabs) {
-    for (int i = 0; i < tabs.size(); i++) {
-      Tab tab = tabs.get(i);
-      if (tab.getValue() > value)
-        return tab;
-    }
-    return null;
-  }
-
-  private <Tab extends Variable> Tab findFirstSmallerTab(double value, List<Tab> tabs) {
-    for (int i = tabs.size() - 1; i >= 0; i--) {
-      Tab tab = tabs.get(i);
-      if (tab.getValue() < value)
-        return tab;
-    }
-    return null;
-  }
-
-  public Area findContentAreaAt(float x, float y) {
-    for (Area area : getAreas()) {
-      if (area.getContentRect().contains(x, y))
-        return area;
-    }
-    return null;
-  }
-
-  public Area findAreaAt(float x, float y) {
-    for (Area area : getAreas()) {
-      if (area.getRect().contains(x, y))
-        return area;
-    }
-    return null;
   }
 }
