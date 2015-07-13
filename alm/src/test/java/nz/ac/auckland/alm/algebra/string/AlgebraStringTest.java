9a5f83e5afbdfa8291eab7dd350c9e2370954e9a
Clemens Zeidler <czei002@aucklanduni.ac.nz>
2015-07-14 11:04:32 +1200

    Add copyright.

--- alm/src/test/java/nz/ac/auckland/alm/algebra/string/AlgebraStringTest.java
+++ alm/src/test/java/nz/ac/auckland/alm/algebra/string/AlgebraStringTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2015.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
 package nz.ac.auckland.alm.algebra.string;
 
 import nz.ac.auckland.alm.*;
