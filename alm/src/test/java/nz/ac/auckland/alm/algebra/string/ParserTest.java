02aa6cd318ddf8f87c2d30bff6bc123c92a0ad92
Clemens Zeidler <czei002@aucklanduni.ac.nz>
2015-07-16 11:45:57 +1200

    Add some more test cases.

--- alm/src/test/java/nz/ac/auckland/alm/algebra/string/ParserTest.java
+++ alm/src/test/java/nz/ac/auckland/alm/algebra/string/ParserTest.java
@@ -10,10 +10,17 @@ package nz.ac.auckland.alm.algebra.string;
 import junit.framework.TestCase;
 import nz.ac.auckland.alm.IArea;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ParserTest extends TestCase {
+    private void assertStringNotParsed(String string) {
+        System.out.println("Parse bad string: " + string);
+        List<IArea> items = StringReader.read(string);
+        assertTrue(items == null);
+    }
+
     private void assertStringParsed(String string) {
         System.out.println("Parse: " + string);
         List<IArea> items = StringReader.read(string);
@@ -27,8 +34,38 @@ public class ParserTest extends TestCase {
         assertStringParsed("((m))");
         assertStringParsed("A");
 
+        assertStringNotParsed("(");
+        assertStringNotParsed("}");
+        assertStringNotParsed("(A");
+        assertStringNotParsed("(A |");
+        assertStringNotParsed("(A | }");
+        assertStringNotParsed("(A: | }");
+
         List<IArea> items = StringReader.read("");
         assertTrue(items != null);
         assertEquals(0, items.size());
+    }
+
+    private void setBorderTabs(List<IArea> fragments) {
+        List<IArea> atoms = toAtoms(fragments);
+        for (IArea atom : atoms) {
+
+
+        }
+    }
+    private List<IArea> toAtoms(List<IArea> fragments) {
+        List<IArea> atoms = new ArrayList<IArea>();
+        for (IArea fragment : fragments)
+            toAtoms(fragment, atoms);
+        return atoms;
+    }
+
+    private void toAtoms(IArea fragment, List<IArea> atoms) {
+        if (!(fragment instanceof Fragment)) {
+            atoms.add(fragment);
+            return;
+        }
+        for (IArea child : (List<IArea>)((Fragment) fragment).getItems())
+            toAtoms(child, atoms);
     }
 }
