04691aa9dba16203b7f4a0976dd1b989f0b8c76f
Clemens Zeidler <czei002@aucklanduni.ac.nz>
2015-07-16 17:57:02 +1200

    Merge parser test into AlgebraStingTest and improve the tests.

--- alm/src/test/java/nz/ac/auckland/alm/algebra/string/AlgebraStringTest.java
+++ alm/src/test/java/nz/ac/auckland/alm/algebra/string/AlgebraStringTest.java
@@ -10,9 +10,147 @@ package nz.ac.auckland.alm.algebra.string;
 import nz.ac.auckland.alm.*;
 import nz.ac.auckland.alm.algebra.AlgebraData;
 import nz.ac.auckland.alm.algebra.BaseAlgebraTestCase;
+import nz.ac.auckland.linsolve.Variable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class AlgebraStringTest extends BaseAlgebraTestCase {
+    private void assertStringNotParsed(String string) {
+        System.out.println("Parse bad string: " + string);
+        List<IArea> items = StringReader.readRawFragments(string, null);
+        assertTrue(items == null);
+    }
+
+    private void assertStringParsed(String string) {
+        System.out.println("Parse: " + string);
+        List<IArea> items = StringReader.readRawFragments(string, null);
+        assertTrue(items != null);
+        System.out.println("Result: " + StringWriter.write(items));
+    }
+
+    public void testParsing() {
+        assertStringParsed("AD2 |{2} B * (A | {  2 } B) ");
+        assertStringParsed("(A|B|C)/ (D|E|((r/b)) ) / A  *m");
+        assertStringParsed("((m))");
+        assertStringParsed("A");
+
+        assertStringNotParsed("(");
+        assertStringNotParsed("}");
+        assertStringNotParsed("(A");
+        assertStringNotParsed("(A |");
+        assertStringNotParsed("(A | }");
+        assertStringNotParsed("(A: | }");
+
+        List<IArea> items = StringReader.readRawFragments("", null);
+        assertTrue(items != null);
+        assertEquals(0, items.size());
+    }
+
+    private void assignAreaIds(List<IArea> areas) {
+        int areaCount = 0;
+        for (IArea area : areas) {
+            final String areaNames = "ABCDEFGHIJKMNOPQRSTUVWXYZ";
+            int letter = areaCount % areaNames.length();
+            int index = areaCount / areaNames.length();
+            String name = "" + areaNames.charAt(letter);
+            if (index > 0)
+                name += index;
+            area.setId(name);
+            areaCount++;
+        }
+    }
+
+    /**
+     * Checks if a AlgebraData describe the same layout.
+     *
+     * The areas are matched by their ids. The AlgebraData's tabstops can be different though. If no matching area is
+     * found false is returned.
+     *
+     * @param algebraData
+     * @param sameBorders check if the border tabstops are the same
+     * @return true if the AlgebraData is equivalent
+     */
+    public boolean isEquivalent(AlgebraData algebraData1,  AlgebraData algebraData, boolean sameBorders) {
+        if (sameBorders) {
+            if (algebraData1.getLeft() != algebraData.getLeft() || algebraData1.getTop() != algebraData.getTop()
+                    || algebraData1.getRight() != algebraData.getRight()
+                    || algebraData1.getBottom() != algebraData.getBottom())
+                return false;
+        }
+        Map<Variable, Variable> oursToTheirsMap = new HashMap<Variable, Variable>();
+        List<IArea> ourAreas = algebraData1.getAllAreasList();
+        List<IArea> theirAreas = algebraData.getAllAreasList();
+        if (ourAreas.size() != theirAreas.size())
+            return false;
+        for (IArea ourArea : ourAreas) {
+            if (ourArea.getId() == null)
+                return false;
+            IArea theirArea = null;
+            for (IArea area : theirAreas) {
+                if (area.getId() != null && area.getId().equals(ourArea.getId())) {
+                    theirArea = area;
+                    break;
+                }
+            }
+            if (theirArea == null)
+                return false;
+            if (!isValidTabMatch(oursToTheirsMap, ourArea.getLeft(), theirArea.getLeft()))
+                return false;
+            if (!isValidTabMatch(oursToTheirsMap, ourArea.getTop(), theirArea.getTop()))
+                return false;
+            if (!isValidTabMatch(oursToTheirsMap, ourArea.getRight(), theirArea.getRight()))
+                return false;
+            if (!isValidTabMatch(oursToTheirsMap, ourArea.getBottom(), theirArea.getBottom()))
+                return false;
+        }
+        return true;
+    }
+
+    static private boolean isValidTabMatch(Map<Variable, Variable> oursToTheirsMap, Variable our, Variable theirs) {
+        Variable existingTheirs = oursToTheirsMap.get(our);
+        if (existingTheirs == null) {
+            oursToTheirsMap.put(our, theirs);
+            return true;
+        }
+        return existingTheirs == theirs;
+    }
+
+    public void testThreeButtons() {
+        LayoutSpec layoutSpec = getLayoutSpec(500, 500);
+
+        XTab left = layoutSpec.getLeft();
+        YTab top = layoutSpec.getTop();
+        XTab right = layoutSpec.getRight();
+        YTab bottom = layoutSpec.getBottom();
+
+        XTab x1 = new XTab();
+        XTab x2 = new XTab();
+
+        layoutSpec.addArea(new Area(left, top, x1, bottom));
+        layoutSpec.addArea(new Area(x1, top, x2, bottom));
+        layoutSpec.addArea(new Area(x2, top, right, bottom));
+
+        assignAreaIds(layoutSpec.getAreas());
+
+        AlgebraData data = new AlgebraData(layoutSpec, null);
+        AlgebraSpec algebraSpec = new AlgebraSpec(data);
+        algebraSpec.compress();
+
+        System.out.println("Three Buttons:");
+        String algebraString = StringWriter.write(algebraSpec);
+        System.out.println(algebraString);
+
+        assertEquals(1, algebraSpec.getFragments().size());
+
+        AlgebraData readAlgebraData = StringReader.read(algebraString, left, top, right, bottom,
+                Parser.getDefaultAreaFactory());
+        assertTrue(readAlgebraData != null);
+        assertEquals(true, isEquivalent(data, readAlgebraData, true));
+    }
+
     public void testPinWheel() {
         LayoutSpec layoutSpec = getLayoutSpec(500, 500);
 
@@ -34,17 +172,22 @@ public class AlgebraStringTest extends BaseAlgebraTestCase {
         layoutSpec.addArea(new Area(left, y1, x1, bottom));
         // middle
         layoutSpec.addArea(new EmptySpace(x1, y1, x2, y2));
-
+        assignAreaIds(layoutSpec.getAreas());
 
         AlgebraData data = new AlgebraData(layoutSpec, null);
-
         AlgebraSpec algebraSpec = new AlgebraSpec(data);
         algebraSpec.compress();
 
         System.out.println("Pin Wheel:");
-        System.out.println(StringWriter.write(algebraSpec));
+        String algebraString = StringWriter.write(algebraSpec);
+        System.out.println(algebraString);
 
         assertEquals(2, algebraSpec.getFragments().size());
+
+        AlgebraData readAlgebraData = StringReader.read(algebraString, left, top, right, bottom,
+                Parser.getDefaultAreaFactory());
+        assertTrue(readAlgebraData != null);
+        assertEquals(true, isEquivalent(data, readAlgebraData, true));
     }
 
     public void testPinWheelExtension() {
